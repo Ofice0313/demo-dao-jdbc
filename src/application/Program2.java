ef066bb8b45cc0a4cf2df0e4bc30bd2ced831418
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-02-03 16:40:42 +0200

    Department Update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -13,9 +13,12 @@ public class Program2 {
 		
 		System.out.println("====== Test 1: DepartmentDao findById =======");
 		Department department = departmentDao.findById(3);
-		
 		System.out.println(department);
 		
+		System.out.println("====== Test 1: DepartmentDao Update =======");
+		department = departmentDao.findById(2);
+		department.setName("Marketing");
+		departmentDao.update(department);
 		
 
 	}
